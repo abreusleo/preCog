9391425460e8407630c1e3961e5affe48c7bedec
Leonardo Abreu <leonardosabreu8@gmail.com>
2023-08-28 19:45:52 -0300

    Fix dependencies

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
--- Api/Api/TestObject.cs
+++ Api/Api/TestObject.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Api;
 
 public class TestObject
