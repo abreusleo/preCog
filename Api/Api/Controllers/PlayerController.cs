ee7514f13e278738c53548860ca78be0572eddaf
Leonardo Abreu <leonardosabreu8@gmail.com>
2023-10-17 20:34:04 -0300

    Add api mocks

---
+++ Api/Api/Controllers/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlayerController : ControllerBase
+{
+    private readonly ILogger<PlayerController> _logger;
+
+    public PlayerController(ILogger<PlayerController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public IActionResult GetPossiblePlayers()
+    {
+        GetPossiblePlayersDto dto = new GetPossiblePlayersDto { Name = "Carlos",  PhotoUrl = "https://images.unsplash.com/photo-1563409236302-8442b5e644df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZHVja3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"};
+        GetPossiblePlayersDto secondDto = new GetPossiblePlayersDto { Name = "Bessa", PhotoUrl = "https://s2-techtudo.glbimg.com/eZA3MfymxGYzGBJK0Ig8EY7ETwA=/0x0:1080x995/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/B/l/bRJmJAQiWdlUY2BWM39w/1.-the-rock.jpg"};
+
+        List<GetPossiblePlayersDto> dtoList = new List<GetPossiblePlayersDto>{ dto, secondDto};
+        
+        return Ok(dtoList);
+    }
+}
---
+++ Api/Api/Dtos/Prediction.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using Api.Enums;
+
+namespace Api.Dtos;
+
+public class Prediction
+{
+    public PredictionTypes Type { get; set; }
+    public JsonElement Input { get; set; }
+}
+
+public class TeamInput
+{
+    public string FirstTeam { get; set; }
+    public string SecondTeam { get; set; }
+}
+
+public class ChampionshipInput
+{
+    public string Name { get; set; }
+}
+
+// TODO: Pensar em como vai funcionar essa predição
+public class PlayerInput
+{
+    public string Name { get; set; }
+}
