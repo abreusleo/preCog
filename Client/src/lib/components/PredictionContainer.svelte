ce9b6c71a253bbaa795fe3103bd2cb8a8646de80
brunobaronims <brunobaronims@gmail.com>
2023-11-07 14:17:42 -0300

    Update desktop layout

--- Client/src/lib/components/PredictionContainer.svelte
+++ Client/src/lib/components/PredictionContainer.svelte
@@ -4,19 +4,21 @@
   import DownScroll from '$lib/assets/page_scroll_arrow.svg';
 </script>
 
-<section class="relative z-10 flex h-full w-full flex-col justify-between pb-24 pt-24">
-  <div class="mb-10 flex w-full flex-col items-center">
-    <h1
-      class="mb-[0.4rem] flex w-full items-center justify-center font-primary text-lg text-light-pink"
-    >
-      Prediction type
-    </h1>
-    <hr class="border-1 mb-6 w-16 border-light-pink bg-light-pink" />
-    <Carousel />
+<section class="relative z-10 flex h-full w-full flex-col justify-between pb-24 pt-8 sm:bg-dark-grey sm:flex-row sm:w-[70%] sm:p-0 sm:my-12 sm:rounded-lg sm:h-[30rem]">
+  <!-- <div class="flex w-full flex-col items-center sm:flex-row">
+    <div class='flex flex-col items-center'>
+      <h1
+        class="mb-[0.4rem] flex w-full items-center justify-center font-primary text-lg text-light-pink"
+      >
+        Prediction type
+      </h1>
+      <hr class="border-1 mb-6 w-16 border-light-pink bg-light-pink" />
+      <Carousel />
+    </div>
     <PredictionSelection />
-    <hr class="border-1 mb-6 w-16 border-light-pink bg-light-pink" />
+    <hr class="border-1 mb-6 w-16 border-light-pink bg-light-pink sm:hidden" />
   </div>
   <div class="flex w-full justify-center">
-    <img src={DownScroll} alt="Down arrow" class="h-auto w-max shadow-black drop-shadow-lg" />
-  </div>
+    <img src={DownScroll} alt="Down arrow" class="h-auto w-max shadow-black drop-shadow-lg sm:hidden" />
+  </div> -->
 </section>
--- Client/src/routes/+page.svelte
+++ Client/src/routes/+page.svelte
@@ -3,17 +3,20 @@
   import PredictionContainer from '$lib/components/PredictionContainer.svelte';
 </script>
 
-<main class="h-screen">
+<main class="h-screen pt-16 flex flex-col items-center">
   <div class="fixed left-0 top-14 z-0 h-screen w-full overflow-hidden">
-    <div class="fixed h-screen opacity-5">
+    <div class="fixed h-screen opacity-5 sm:opacity-100">
       <img
         src={BackgroundImg}
         alt="Omen background"
-        class="fixed h-full w-auto object-cover"
+        class="fixed h-full w-auto object-cover sm:w-full"
         width="2560"
         height="1440"
       />
     </div>
   </div>
   <PredictionContainer />
+  <div class='hidden sm:flex w-full bg-dark-grey h-full relative z-10'>
+
+  </div>
 </main>
