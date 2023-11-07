7d60446938e884d416077a362d43cb83e6d96418
Carlos Ribeiro <carlos.rib.rocha@gmail.com>
2023-11-07 20:08:27 -0300

    Sign in button added to screen, but not working

--- Client/src/lib/components/Header/Header.svelte
+++ Client/src/lib/components/Header/Header.svelte
@@ -13,21 +13,28 @@
   class="fixed left-0 top-0 z-40 flex h-16 w-full items-center justify-between bg-dark-grey px-4 shadow-md shadow-black"
 >
   <img src={Logo} alt="PreCog logo" />
-  <button class="flex flex-col items-center justify-between" on:click={toggleMenuOpen}>
-    <span
-      class={fadeOutHeaderButton +
-        fadeInHeaderButton +
-        'mb-1 h-1 w-auto rounded-full border-[2.5px] border-light-pink'}
-    />
-    <span
-      class={fadeOutHeaderButton +
-        fadeInHeaderButton +
-        'mb-1 h-1 w-auto rounded-full border-[2.5px] border-light-pink [animation-delay:100ms]'}
-    />
-    <span
-      class={fadeOutHeaderButton +
-        fadeInHeaderButton +
-        'h-1 w-auto rounded-full border-[2.5px] border-light-pink [animation-delay:200ms]'}
-    />
-  </button>
+  <div class="flex gap-4">
+    <button
+      class="flex h-12 w-28 items-center justify-center rounded-md bg-dark-grey font-primary text-base text-light-pink border border-gray-600"
+    >
+      SIGN IN
+    </button>
+    <button on:click={toggleMenuOpen}>
+      <span
+        class={fadeOutHeaderButton +
+          fadeInHeaderButton +
+          'mb-1 h-1 w-auto rounded-full border-[2.5px] border-light-pink'}
+      />
+      <span
+        class={fadeOutHeaderButton +
+          fadeInHeaderButton +
+          'mb-1 h-1 w-auto rounded-full border-[2.5px] border-light-pink [animation-delay:100ms]'}
+      />
+      <span
+        class={fadeOutHeaderButton +
+          fadeInHeaderButton +
+          'h-1 w-auto rounded-full border-[2.5px] border-light-pink [animation-delay:200ms]'}
+      />
+    </button>
+  </div>
 </div>
