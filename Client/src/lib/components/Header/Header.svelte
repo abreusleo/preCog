44d1e1da5aeeb8b66f738f5517ba3ab786c62596
Carlos Ribeiro <carlos.rib.rocha@gmail.com>
2023-11-12 18:06:07 -0300

    On clicking button, auth modal must be displayed

--- Client/src/lib/components/Header/Header.svelte
+++ Client/src/lib/components/Header/Header.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import Logo from '$lib/assets/precoglogo.svg';
+  import Modal from '../Modal/Modal.svelte';
 
   export let headerButtonFadingOut = false;
   export let headerButtonVisible = true;
   export let toggleMenuOpen = () => {null};
+
+  let showModal = false;
+
+  function toggleModalOpen() {
+    showModal = true;
+  }
 
   $: fadeOutHeaderButton = headerButtonFadingOut ? 'animate-fade-out-header-button ' : ' ';
   $: fadeInHeaderButton = headerButtonVisible ? 'animate-fade-in-header-button block ' : 'hidden ';
@@ -16,6 +23,7 @@
   <div class="flex gap-4">
     <button
       class="flex h-12 w-28 items-center justify-center rounded-md bg-dark-grey font-primary text-base text-light-pink border border-gray-600"
+      on:click={toggleModalOpen}
     >
       SIGN IN
     </button>
@@ -38,3 +46,30 @@
     </button>
   </div>
 </div>
+
+<Modal bind:showModal>
+  <div slot="body" class="grid grid-cols-1 content-center">
+    <form>
+      <div class="m-5">
+        <label for="username" class="text-xl">Username</label>
+        <input type="text" id="username" class="bg-black text-white text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 block w-full p-2.5" required>
+      </div>
+      <div class="m-5">
+        <label for="password" class="text-xl">Password</label>
+        <input type="password" name="password" id="password" class="bg-black text-white text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 block w-full p-2.5" required>
+      </div>
+      <div class="m-5">
+        <button
+          class="h-12 w-28 rounded-md bg-red-500 font-primary text-sm text-light-pink"
+        >
+          ENTER
+        </button>
+      </div>
+    </form>
+  </div>
+  <div slot="footer">
+    <div class="font-primary text-sm text-light-pink">
+        Don't have an account? &nbsp;<a class="underline text-red-700" href="">Sign Up</a>
+    </div>
+  </div>
+</Modal>
---
+++ Client/src/lib/components/Modal/Modal.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	export let showModal: Boolean; // boolean
+
+	let dialog: HTMLDialogElement; // HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<slot name="header" />
+		<slot name="body" />
+		<slot name="footer" />
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		max-width: 32em;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+        background-color: theme('colors.dark-grey');
+        color: theme('colors.light-pink');
+        text-align:center;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+	dialog > div {
+		padding: 1em;
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: block;
+	}
+</style>
