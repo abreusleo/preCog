9ea5a7e1b2c868b073a8f402fbc0f5c8a1b416ea
Leonardo Abreu <leonardosabreu8@gmail.com>
2024-02-16 19:23:30 -0300

    Fix method names + moved some imports for the specific class (PR changes)

--- Api/Api.UnitTests/PredictorFactoryTests.cs
+++ Api/Api.UnitTests/PredictorFactoryTests.cs
@@ -1,10 +1,15 @@
+using Api.Enums;
+using Api.Services.Predictors;
+using Api.Services.Predictors.Implementations;
+using Api.Exceptions;
+
 namespace Api.UnitTests;
 
-public class Fixture
+public abstract class Fixture
 {
     public IPredictorFactory Factory { get; set; }
 
-    public Fixture()
+    protected Fixture()
     {
         Mock<ILogger<Predictor>> logger = new();
         Factory = new PredictorFactory(logger.Object);
@@ -14,38 +19,38 @@ public class Fixture
 
 public class PredictorFactoryTests : IClassFixture<Fixture>
 {
-    private readonly IPredictorFactory _factory;
+    private readonly IPredictorFactory _predictorFactory;
 
     public PredictorFactoryTests(Fixture fixture)
     {
-        _factory = fixture.Factory;
+        _predictorFactory = fixture.Factory;
     }
 
     [Fact]
     public void CreatePredictor_ChampionshipType_ReturnsChampionshipPredictor()
     {
         // Setup
-        Predictor predictor = _factory.Create(PredictionTypes.Championships);
+        Predictor predictor = _predictorFactory.Create(PredictionTypes.Championships);
         
         // Assert
         Assert.Equal(typeof(ChampionshipPredictor), predictor.GetType());
     }
     
     [Fact]
-    public void CreatePredictor_TeamsType_ReturnsChampionshipPredictor()
+    public void CreatePredictor_TeamsType_ReturnsTeamPredictor()
     {
         // Setup
-        Predictor predictor = _factory.Create(PredictionTypes.Teams);
+        Predictor predictor = _predictorFactory.Create(PredictionTypes.Teams);
         
         // Assert
         Assert.Equal(typeof(TeamPredictor), predictor.GetType());
     }
     
     [Fact]
-    public void CreatePredictor_PlayerType_ReturnsChampionshipPredictor()
+    public void CreatePredictor_PlayerType_ReturnsPlayerPredictor()
     {
         // Setup
-        Predictor predictor = _factory.Create(PredictionTypes.Players);
+        Predictor predictor = _predictorFactory.Create(PredictionTypes.Players);
         
         // Assert
         Assert.Equal(typeof(PlayerPredictor), predictor.GetType());
@@ -57,6 +62,6 @@ public class PredictorFactoryTests : IClassFixture<Fixture>
         // Setup
         const int unexpectedType = int.MaxValue;
         // Assert
-        Assert.Throws<PredictionTypeNotFoundException>(() => _factory.Create((PredictionTypes) unexpectedType));
+        Assert.Throws<PredictionTypeNotFoundException>(() => _predictorFactory.Create((PredictionTypes) unexpectedType));
     }
 }
--- Api/Api.UnitTests/Usings.cs
+++ Api/Api.UnitTests/Usings.cs
@@ -1,7 +1,3 @@
 global using Xunit;
 global using Moq;
-global using Api.Enums;
-global using Api.Services.Predictors;
-global using Api.Services.Predictors.Implementations;
-global using Api.Exceptions;
 global using Microsoft.Extensions.Logging;
