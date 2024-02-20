412b9a6216bfa2f74e1c79fe2e44c31567316d1c
Leonardo Abreu <leonardosabreu8@gmail.com>
2024-02-19 21:35:15 -0300

    add grpc insecure configuration

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -5,14 +5,16 @@ using Microsoft.Extensions.Hosting;
 using System.IO;
 using System.Reflection;
 using System;
+using System.Net.Http;
+using System.Net.Security;
+using System.Threading;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddGrpcClient<PredictorGrpc.PredictorGrpcClient>(o => o.Address = new Uri("dns:///predictor:50051")).ConfigurePrimaryHttpMessage(() => return new HttpClientHandler{ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator};);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(config =>
@@ -28,6 +30,23 @@ builder.Services.AddSwaggerGen(config =>
 
 // PreCog Services
 builder.Services.AddScoped<PredictorFactory>();
+
+// Grpc
+builder.Services.AddGrpcClient<PredictorGrpc.PredictorGrpcClient>(o =>
+{
+    o.Address = new Uri("dns:///predictor:50051");
+    o.ChannelOptionsActions.Add((Action<GrpcChannelOptions>) (opt =>
+    {
+        opt.HttpHandler = (HttpMessageHandler) new SocketsHttpHandler()
+        {
+            EnableMultipleHttp2Connections = true,
+            PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+            KeepAlivePingDelay = TimeSpan.FromSeconds(60.0),
+            KeepAlivePingTimeout = TimeSpan.FromSeconds(60.0)
+        };
+        opt.Credentials = ChannelCredentials.Insecure; 
+    }));
+});
 
 var app = builder.Build();
 
