cf95c7b544216c9c69520a4cd9dcb513dbe76735
Leonardo Abreu <leonardosabreu8@gmail.com>
2024-03-11 19:13:47 -0300

    Remove useless usings

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Api;
--- Api/Api/Services/Predictors/Implementations/PlayerPredictor.cs
+++ Api/Api/Services/Predictors/Implementations/PlayerPredictor.cs
@@ -2,7 +2,6 @@ using System;
 using System.Text.Json;
 using Api.Dtos;
 using Api.Exceptions;
-using Api.Storage;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
